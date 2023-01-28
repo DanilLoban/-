68a727496694a45872afeffc87c3b214489b654e
Danil <dan.loban@yandex.ru>
2023-01-28 23:40:47 +0300

    finish

---
+++ Cod/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] array1 = new string[7] { "Sunday", "Mon", "Tuesday", "We", "Thursday", "F", "Saturday" };
+string[] array2 = new string[array1.Length];
+void StringArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for(int i =0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for(int i =0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+StringArray(array1, array2);
+PrintArray(array2);
